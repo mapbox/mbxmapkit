ac39283ea9c21ccd42088f1d37768f7067b4e296
Justin R. Miller <incanus@codesorcery.net>
2013-09-17 11:08:15 -0700

    initial commit

---
+++ docs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+VERSION=$( git tag | sort -r | sed -n '1p' )
+
+appledoc \
+    --output Docs \
+    --project-name "MBXMapKit $VERSION" \
+    --project-company MapBox \
+    --create-html \
+    --create-docset \
+    --company-id com.mapbox \
+    --ignore .m \
+    --clean-output \
+    --docset-install-path /tmp/docset \
+    --index-desc README.md \
+    --docset-atom-filename docset.atom \
+    --docset-feed-url "http://mapbox.com/mbxmapkit/Docs/publish/%DOCSETATOMFILENAME" \
+    --docset-package-url "https://github.com/mapbox/mbxmapkit/raw/packaging/Downloads/%DOCSETPACKAGEFILENAME" \
+    --publish-docset \
+    .
