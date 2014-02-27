mapbox/mbxmapkit

commit 0affd03869e6ba5aef996743a2c5ff49216ba861

use marker size prefixes; some code brevity

MBXMapKit.m
@@ -93,34 +93,26 @@ typedef NS_ENUM(NSUInteger, MBXMapViewShowDefaultBaseLayerMode) {
     // See https://www.mapbox.com/developers/api/#Stand-alone.markers
     //
     NSMutableString *marker = [[NSMutableString alloc] initWithString:@"pin-"];
-    if ([@"small" isEqualToString:size])
+
+    if ([size hasPrefix:@"l"])
     {
-        [marker appendString:@"s-"];
+        [marker appendString:@"l-"]; // large
     }
-    else if ([@"medium" isEqualToString:size])
+    if ([size hasPrefix:@"s"])
     {
-        [marker appendString:@"m-"];
-    }
-    else if ([@"large" isEqualToString:size])
-    {
-        [marker appendString:@"l-"];
+        [marker appendString:@"s-"]; // small
     }
     else
     {
-        // Default to a medium sized icon if the simplestyle GeoJSON specified no size or an unexpected size string
-        //
-        [marker appendString:@"m-"];
+        [marker appendString:@"m-"]; // default to medium
     }
+
     [marker appendFormat:@"%@+",symbol];
+
     [marker appendString:[color stringByReplacingOccurrencesOfString:@"#" withString:@""]];
-    if([UIScreen mainScreen].scale == 2.0)
-    {
-        [marker appendString:@"@2x.png"];
-    }
-    else
-    {
-        [marker appendString:@".png"];
-    }
+
+    [marker appendString:([[UIScreen mainScreen] scale] > 1.0 ? @"@2x.png" : @".png")];
+
     return marker;
 }
 

commit cea078e4525167b9ace6fbd2b56ea4a77ade1ee9

Fix #56 by only writing HTTP responses to cache. Also fixes the double-size-prefix bug in the marker url which was introduced by 0affd03869.

MBXMapKit.m
@@ -98,7 +98,7 @@ typedef NS_ENUM(NSUInteger, MBXMapViewShowDefaultBaseLayerMode) {
     {
         [marker appendString:@"l-"]; // large
     }
-    if ([size hasPrefix:@"s"])
+    else if ([size hasPrefix:@"s"])
     {
         [marker appendString:@"s-"]; // small
     }
@@ -143,7 +143,10 @@ typedef NS_ENUM(NSUInteger, MBXMapViewShowDefaultBaseLayerMode) {
             // At this point we should have an NSHTTPURLResponse with an HTTP 200, or else an
             // NSURLResponse with the contents of a file from cache. Both of those are good.
             //
-            [data writeToFile:makiPinCachePath atomically:YES];
+            if ([response isKindOfClass:[NSHTTPURLResponse class]])
+            {
+                [data writeToFile:makiPinCachePath atomically:YES];
+            }
 
             self.image = [[UIImage alloc] initWithData:data scale:[[UIScreen mainScreen] scale]];
 
@@ -341,12 +344,15 @@ typedef NS_ENUM(NSUInteger, MBXMapViewShowDefaultBaseLayerMode) {
                     }
                     else
                     {
-                        // Cache to disk in folders sorted by mapID.
-                        //
-                        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^(void)
+                        if ([response isKindOfClass:[NSHTTPURLResponse class]])
                         {
-                            [data writeToFile:[self cachePathForTilePath:path] atomically:YES];
-                        });
+                            // Cache tiles which came from an HTTP 200 response to disk in folders sorted by mapID.
+                            //
+                            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^(void)
+                            {
+                                [data writeToFile:[self cachePathForTilePath:path] atomically:YES];
+                            });
+                        }
                     }
 
                     // Return the new tile data.
@@ -738,7 +744,12 @@ typedef NS_ENUM(NSUInteger, MBXMapViewShowDefaultBaseLayerMode) {
                                                         }
                                                     }
 
-                                                    [data writeToFile:tileJSONCachePath atomically:YES];
+                                                    // Cache the tileJSON only if it came from an HTTP 200 response
+                                                    //
+                                                    if ([response isKindOfClass:[NSHTTPURLResponse class]])
+                                                    {
+                                                        [data writeToFile:tileJSONCachePath atomically:YES];
+                                                    }
 
                                                     dispatch_sync(dispatch_get_main_queue(), ^(void)
                                                     {
@@ -854,7 +865,10 @@ typedef NS_ENUM(NSUInteger, MBXMapViewShowDefaultBaseLayerMode) {
 
                                                if (markersJSONDictionary)
                                                {
-                                                   [data writeToFile:markersJSONCachePath atomically:YES];
+                                                   if ([response isKindOfClass:[NSHTTPURLResponse class]])
+                                                   {
+                                                       [data writeToFile:markersJSONCachePath atomically:YES];
+                                                   }
 
                                                    dispatch_sync(dispatch_get_main_queue(), ^(void)
                                                    {