d6ae6daf82ee1e4d96e699230e6a4458efc974b4
Will Snook <wsnook@users.noreply.github.com>
2014-03-06 13:46:36 -0700

    #61 Mac demo app is done (matches iOS demo app)

--- Sample Project/MBXMapKit Mac Demo v030/MBXzAppDelegate.m
+++ Sample Project/MBXMapKit Mac Demo v030/MBXzAppDelegate.m
@@ -52,8 +52,97 @@
     _simplestyle.mapID = @"examples.map-pgygbwdm";
 
     [_mapView addOverlay:_rasterOverlay];
+
+    // Configure the popup button for selecting which map to show
+    //
+    NSArray *titles = [NSArray arrayWithObjects:@"OSM world map",@"OSM over Apple satellite",@"Terrain under Apple labels",@"Tilemill bounded region",@"Tilemill region over Apple",@"Tilemill transparent over Apple", nil];
+    [_popupButton removeAllItems];
+    [_popupButton addItemsWithTitles:titles];
 }
 
+
+- (void)resetMapViewAndRasterOverlayDefaults
+{
+    // This method prepares the MKMapView to switch overlays. Note that the specific order
+    // in which things happen is quite important. One of the goals here is to fully disconnect old
+    // tile overlays, annotations, and simplestyle prior to adding their new replacements. The
+    // consequences of not doing that could potentially include stuff like EXEC_BAD_ACCESS, so it's
+    // good to handle these things carefully.
+    //
+    _mapView.mapType = MKMapTypeStandard;
+
+    // Set up a new tile overlay to account for the possibility that there are still tiles or TileJSON being downloaded
+    //
+    _rasterOverlay.delegate = nil;
+    [_mapView removeOverlays:_mapView.overlays];
+    _rasterOverlay = [[MBXRasterTileOverlay alloc] init];
+    _rasterOverlay.delegate = _standardDelegate;
+
+    // Set up a new simplestyle object to account for the possibility that there are still point icons being downloaded
+    //
+    _simplestyle.delegate = nil;
+    [_mapView removeAnnotations:_mapView.annotations];
+    _simplestyle = [[MBXSimplestyle alloc] init];
+    _simplestyle.delegate = _standardDelegate;
+
+    _mapView.scrollEnabled = YES;
+    _mapView.zoomEnabled = YES;
+    _rasterOverlay.canReplaceMapContent = YES;
+}
+
+
+
+- (IBAction)popupButtonAction:(id)sender {
+    if ([sender isKindOfClass:[NSPopUpButton class]])
+    {
+        NSPopUpButton *popup = (NSPopUpButton *)sender;
+        switch([popup indexOfSelectedItem])
+        {
+            case 0:
+                // OSM world map
+                [self resetMapViewAndRasterOverlayDefaults];
+                _rasterOverlay.mapID = _simplestyle.mapID = @"examples.map-pgygbwdm";
+                [_mapView addOverlay:_rasterOverlay];
+                break;
+            case 1:
+                // OSM over Apple satellite
+                [self resetMapViewAndRasterOverlayDefaults];
+                _mapView.mapType = MKMapTypeSatellite;
+                _rasterOverlay.canReplaceMapContent = NO;
+                _rasterOverlay.mapID = @"justin.map-9tlo4knw";
+                [_mapView addOverlay:_rasterOverlay];
+                break;
+            case 2:
+                // Terrain under Apple labels
+                [self resetMapViewAndRasterOverlayDefaults];
+                _rasterOverlay.mapID = @"justin.map-mf07hryq";
+                [_mapView insertOverlay:_rasterOverlay atIndex:0 level:MKOverlayLevelAboveRoads];
+                break;
+            case 3:
+                // Tilemill bounded region (scroll & zoom limited to programmatic control only)
+                [self resetMapViewAndRasterOverlayDefaults];
+                _mapView.scrollEnabled = NO;
+                _mapView.zoomEnabled = NO;
+                _rasterOverlay.mapID = @"justin.NACIS2012";
+                [_mapView addOverlay:_rasterOverlay];
+                break;
+            case 4:
+                // Tilemill region over Apple
+                [self resetMapViewAndRasterOverlayDefaults];
+                _rasterOverlay.canReplaceMapContent = NO;
+                _rasterOverlay.mapID = @"justin.clp-2011-11-03-1200";
+                [_mapView addOverlay:_rasterOverlay];
+                break;
+            case 5:
+                // Tilemill transparent over Apple
+                [self resetMapViewAndRasterOverlayDefaults];
+                _rasterOverlay.canReplaceMapContent = NO;
+                _rasterOverlay.mapID = @"justin.pdx_meters";
+                [_mapView addOverlay:_rasterOverlay];
+                break;
+        }
+    }
+}
 
 
 @end
