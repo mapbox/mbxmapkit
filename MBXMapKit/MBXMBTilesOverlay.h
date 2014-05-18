ec2234742452facc019a416c1e8fd2ce58c52b60
Will Snook <wsnook@users.noreply.github.com>
2014-05-18 03:48:31 -0600

    New branch for working on #3

---
+++ MBXMapKit/MBXMBTilesOverlay.h
@@ -0,0 +1,13 @@
+//
+//  MBXMBTilesOverlay.h
+//  MBXMapKit
+//
+//  Copyright (c) 2014 Mapbox. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface MBXMBTilesOverlay : MKTileOverlay
+
+
+@end
---
+++ MBXMapKit/MBXMBTilesOverlay.m
@@ -0,0 +1,13 @@
+//
+//  MBXMBTilesOverlay.m
+//  MBXMapKit
+//
+//  Copyright (c) 2014 Mapbox. All rights reserved.
+//
+
+#import "MBXMBTilesOverlay.h"
+
+@implementation MBXMBTilesOverlay
+
+
+@end
