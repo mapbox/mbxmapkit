c14a49e7f66ae6b1e5d039bd02ab5b1eaae8d7de
Justin R. Miller <incanus@codesorcery.net>
2014-06-11 17:00:38 -0700

    refs #85: clean up MBXOfflineMapDatabase docs

--- MBXMapKit/MBXOfflineMapDatabase.h
+++ MBXMapKit/MBXOfflineMapDatabase.h
@@ -11,33 +11,40 @@
 
 #pragma mark -
 
-/** A runtime object providing access to a database of persistent map data stored on disk. These objects are managed by MBXOfflineMapDownloader and are not meant to be directly instantiated by other means. */
+/** An instance of the MBXOfflineMapDatabase class represents a store of offline map data, including map tiles, JSON metadata, and marker images. 
+*
+*   @warning The MBXOfflineMapDatabase class is not meant to be instantiated directly. Instead, instances are created and managed by the shared MBXOfflineMapDownloader instance. */
 @interface MBXOfflineMapDatabase : NSObject
 
 
 #pragma mark - Properties and methods for accessing stored map data
 
-/** @name Properties and methods for accessing stored map data */
+/** @name Getting and Setting Properties */
 
-/** The map ID from which the map resources in this offline map were downloaded */
+/** The map ID from which the map resources in this offline map were downloaded. */
 @property (readonly, nonatomic) NSString *mapID;
-/** Whether this offline map database includes the map's metadata JSON */
+
+/** Whether this offline map database includes the map's metadata JSON. */
 @property (readonly, nonatomic) BOOL includesMetadata;
-/** Whether this offline map database includes the map's markers JSON and marker icons */
+
+/** Whether this offline map database includes the map's markers JSON and marker icons. */
 @property (readonly, nonatomic) BOOL includesMarkers;
-/** The image quality used to download the tile URLs stored in this offline map database */
+
+/** The image quality used to download the raster tile images stored in this offline map database. */
 @property (readonly, nonatomic) MBXRasterImageQuality imageQuality;
-/** The map region which was used to initiate the downloading of the tiles in this offline map database */
+
+/** The map region which was used to initiate the downloading of the tiles in this offline map database. */
 @property (readonly, nonatomic) MKCoordinateRegion mapRegion;
-/** The minimum zoom limit which was used to initiate the downloading of the tiles in this offline map database */
+
+/** The minimum zoom limit which was used to initiate the downloading of the tiles in this offline map database. */
 @property (readonly, nonatomic) NSInteger minimumZ;
-/** The maximum zoom limit which was used to initiate the downloading of the tiles in this offline map database */
+
+/** The maximum zoom limit which was used to initiate the downloading of the tiles in this offline map database. */
 @property (readonly, nonatomic) NSInteger maximumZ;
-/** Whether this offline map database has been invalidated. This is to help prevent the completion handler in MBXRasterTileOverlay's  tileAtPath:result: from causing problems when overlay layers are removed from an MKMapView. */
+
+/** Whether this offline map database has been invalidated. This is to help prevent the completion handlers in MBXRasterTileOverlay from causing problems after overlay layers are removed from an `MKMapView`. */
 @property (readonly, nonatomic, getter=isInvalid) BOOL invalid;
 
-
-/** Please use MBXOfflineMapDownloader to manage MBXOfflineMapDatabase objects. */
 - (instancetype)init __attribute__((unavailable("To instantiate MBXOfflineMapDatabase objects, please use the capabilities provided by MBXOfflineMapDownloader.")));
 
 @end
