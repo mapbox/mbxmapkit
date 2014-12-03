7d06bee7b52675fe67339f4b9e7313f508891427
Justin R. Miller <incanus@codesorcery.net>
2014-12-03 12:20:43 -0800

    properly render 512px tiles by chopping parent; bring back iOS 8 support

--- MBXMapKit/MBXRasterTileRenderer.m
+++ MBXMapKit/MBXRasterTileRenderer.m
@@ -28,9 +28,10 @@ const NSUInteger MBXRasterTileRendererLRUCacheSize = 50;
 
 - (MKTileOverlayPath)pathForMapRect:(MKMapRect)mapRect zoomScale:(MKZoomScale)zoomScale {
     MKTileOverlay *tileOverlay = (MKTileOverlay *)self.overlay;
+    CGFloat factor = tileOverlay.tileSize.width / 256;
 
-    NSInteger x = mapRect.origin.x * zoomScale / tileOverlay.tileSize.width;
-    NSInteger y = mapRect.origin.y * zoomScale / tileOverlay.tileSize.width;
+    NSInteger x = round(mapRect.origin.x * zoomScale / (tileOverlay.tileSize.width / factor));
+    NSInteger y = round(mapRect.origin.y * zoomScale / (tileOverlay.tileSize.width / factor));
     NSInteger z = log2(zoomScale) + 20;
 
     MKTileOverlayPath path = {
@@ -53,7 +54,14 @@ const NSUInteger MBXRasterTileRendererLRUCacheSize = 50;
 }
 
 - (BOOL)canDrawMapRect:(MKMapRect)mapRect zoomScale:(MKZoomScale)zoomScale {
+    MKTileOverlay *tileOverlay = (MKTileOverlay *)self.overlay;
     MKTileOverlayPath path = [self pathForMapRect:mapRect zoomScale:zoomScale];
+    BOOL bigTiles = (tileOverlay.tileSize.width / 256 > 1);
+    if (bigTiles) {
+        path.x /= 2;
+        path.y /= 2;
+        path.z -= 1;
+    }
     NSString *xyz = [self xyzForPath:path];
     BOOL tileReady = NO;
 
@@ -84,18 +92,44 @@ const NSUInteger MBXRasterTileRendererLRUCacheSize = 50;
 }
 
 - (void)drawMapRect:(MKMapRect)mapRect zoomScale:(MKZoomScale)zoomScale inContext:(CGContextRef)context {
+    MKTileOverlay *tileOverlay = (MKTileOverlay *)self.overlay;
     CGRect rect = [self rectForMapRect:mapRect];
-    NSString *xyz = [self xyzForPath:[self pathForMapRect:mapRect zoomScale:zoomScale]];
+    MKTileOverlayPath path = [self pathForMapRect:mapRect zoomScale:zoomScale];
+    BOOL bigTiles = (tileOverlay.tileSize.width / 256 > 1);
+    MKTileOverlayPath childPath = path;
+    if (bigTiles) {
+        path.x /= 2;
+        path.y /= 2;
+        path.z -= 1;
+    }
+    NSString *xyz = [self xyzForPath:path];
     UIImage *image = nil;
+    BOOL success = NO;
 
     @synchronized(self) {
         NSUInteger index = [[self.tiles valueForKeyPath:@"xyz"] indexOfObject:xyz];
-        if (index == NSNotFound) return;
-        NSDictionary *tile = self.tiles[index];
-        [self.tiles removeObject:tile];
-        image = [UIImage imageWithData:tile[@"data"]];
-        if (image == nil) return;
-        [self.tiles addObject:tile];
+        if (index != NSNotFound) {
+            NSDictionary *tile = self.tiles[index];
+            [self.tiles removeObject:tile];
+            image = [UIImage imageWithData:tile[@"data"]];
+            if (image != nil) {
+                [self.tiles addObject:tile];
+                success = YES;
+            }
+        }
+    }
+
+    if (!success) {
+        return [self setNeedsDisplayInMapRect:mapRect zoomScale:zoomScale];
+    }
+
+    if (bigTiles) {
+        CGFloat childSize = image.size.width / 2;
+        CGRect cropRect = CGRectMake(0, 0, childSize, childSize);
+        if (childPath.x > path.x * 2) cropRect.origin.x += childSize;
+        if (childPath.y > path.y * 2) cropRect.origin.y += childSize;
+        CGImageRef croppedImage = CGImageCreateWithImageInRect(image.CGImage, cropRect);
+        image = [UIImage imageWithCGImage:croppedImage];
     }
 
     UIGraphicsPushContext(context);
