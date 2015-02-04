db397404a94173bd4adce451d9ef1c39d1c3a3b4
Justin R. Miller <incanus@codesorcery.net>
2015-02-03 16:15:33 -0800

    version bumps for 0.5.0 -> 0.6.0

--- MBXMapKit/MBXMapKit.m
+++ MBXMapKit/MBXMapKit.m
@@ -15,7 +15,7 @@
 
 #import "MBXMapKit.h"
 
-NSString *const MBXMapKitVersion = @"0.5.0";
+NSString *const MBXMapKitVersion = @"0.6.0";
 
 #pragma mark - Add support to MKMapView for using Mapbox-style center/zoom to configure the visible region
 
